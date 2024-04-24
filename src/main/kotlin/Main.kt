b4ad8b2877bccf16d91a26279692088dd94a6900
ftrugon <fjtrujillo@iesrafaelalberti.es>
2024-04-24 18:02:46 +0200

    lo de diego ._.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,6 +11,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
@@ -23,34 +25,52 @@ import androidx.compose.ui.window.rememberWindowState
 @Composable
 @Preview
 fun App() {
-
-    var text by remember { mutableStateOf("Login") }
-
+    var user by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val buttonEnabled = user.isNotBlank() && password.isNotBlank()
 
     MaterialTheme{
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.spacedBy(10.dp, alignment = Alignment.CenterVertically)
         ) {
 
 
             OutlinedTextField(
-                value = "",
+                value = user,
                 label = { Text("Usuario") },
-                onValueChange = {}
+                onValueChange = {user = it}
             )
+
+            var pasVisible by remember { mutableStateOf(false) }
 
             OutlinedTextField(
-                value = "",
+                value = password,
                 label = { Text("Contraseña") },
-                onValueChange = {}
+                onValueChange = {password = it},
+                visualTransformation = if (pasVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    IconToggleButton(
+                        checked = pasVisible,
+                        onCheckedChange = {pasVisible = it}
+                    ){
+                        Icon(
+                            imageVector = if (pasVisible) Icons.Default.VisibilityOff else Icons.Default.Visibility,
+                            contentDescription = null
+                        )
+                    }
+                }
             )
 
-            Button(onClick = {
-                text = "Logeando!"
-            }) {
-                Text(text)
+            Button(
+                onClick = {
+                    user = ""
+                    password = ""
+                },
+                enabled = buttonEnabled
+            ) {
+                Text("Login")
             }
         }
     }
